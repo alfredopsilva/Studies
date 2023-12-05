3e5d5447fdf16bfedce27b24b37ada106f83bbc5
Alfredo Silva <a.alfredops@gmail.com>
2023-12-04 21:39:25 -0500

    Starting DatingApp Project
    Stack
    API -> C# - ASP.net
    Client -> Typescript - Angular

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -12,13 +12,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors(); 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
