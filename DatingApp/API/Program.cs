2e523b05fdc007d5a56ef0387a1bf5a9167551b4
Alfredo Silva <a.alfredops@gmail.com>
2023-12-09 11:09:24 -0500

    Starting Error Handling

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -1,5 +1,5 @@
-using System.Text;
 using API.Extensions;
+using API.Middleware;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +13,8 @@ builder.Services.AddIdentitySevices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthentication(); 
--- DatingApp/client/src/app/app-routing.module.ts
+++ DatingApp/client/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { authGuard } from './_guards/auth.guard';
+import { TestErrorComponent } from './errors/test-error/test-error.component';
 
 const routes: Routes = [
   {
@@ -35,6 +36,7 @@ const routes: Routes = [
       }
     ]
   },
+  {path: 'errors', component: TestErrorComponent},
   {
     path: '**',
     component: HomeComponent,
