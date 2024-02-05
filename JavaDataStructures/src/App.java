1b55b21096ecf86a4444475e95f8f7a24992e5bb
alfredo <alfredo@alfredos-mbp.home>
2024-02-05 09:40:27 -0500

    Java Data Structures Started

---
+++ JavaDataStructures/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        LinkedList LL = new LinkedList(10); 
+
+        System.out.println("LinkedList ");
+        LL.printList();
+    }
+}
