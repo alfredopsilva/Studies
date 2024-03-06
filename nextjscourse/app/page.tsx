0fc413c7833e8d99a816ed6fe7f98e191cadac6d
Alfredo Silva <a.alfredops@gmail.com>
2024-03-06 10:28:09 -0500

    Syncing NextJS Course Progress

--- nextjscourse/app/page.tsx
+++ nextjscourse/app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
-import Banner from "../components/banner.client.tsx"
+import Banner from "../components/banner.client"
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Banner />
+      <Banner buttonText="Discover Coffees near by" handleOnClick={() => { console.log("Hello Button") }} />
     </main>
   );
 }
--- nextjscourse/components/banner.client.tsx
+++ nextjscourse/components/banner.client.tsx
@@ -1,15 +1,35 @@
 "use client"
+import Image from "next/image";
+import { MouseEventHandler } from "react";
+import Hero from "../public/static/hero-image.png"
 
 
-export default function Banner() {
+type BannerProps = {
+  handleOnClick: MouseEventHandler<HTMLButtonElement> | undefined;
+  buttonText: string;
+}
 
-  const handleOnClick = () => {};
+
+export default function Banner({ handleOnClick, buttonText }: BannerProps) {
 
   return (
-    <div>
-      <h1>Coffee Connoisseur</h1>
-      <h2>Discover your local coffee shops!</h2>
-      <button onClick={handleOnClick}>New Stores Near By</button>
+    <div className="mb-12 grid lg:mb-24 lg:grid-cols-2">
+      <div className="z-20 flex flex-col px-2 md:pt-12">
+        <h1 className="my-2 flex-wrap">
+          <span className="pr-2 text-white">Coffee</span>
+          <span className="text-gray-900">Connoisseur</span>
+        </h1>
+        <p className="font-sans text-xl font-semibold text-gray-900 md:mt-5 lg:text-2xl">
+          Discover your local coffee shops!
+        </p>
+
+        <div className="mt-12">
+          <button onClick={handleOnClick}>{buttonText}</button>
+        </div>
+      </div>
+      <div className="absolute top-2 z-10 md:top-0 md:mt-12 md:pl-10 md:pt-0 lg:right-1/4 lg:flex lg:pl-20">
+        <Image src={Hero} width={800} height={300} alt="hero image" priority={true} />
+      </div>
     </div>
   )
 }
